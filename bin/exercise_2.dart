7860cbf23af891a1b06f598b8be2e15b28ad40e7
mohamed ibrahem <mohamed.data13@gmail.com>
2023-03-27 04:39:29 +0200

    best solution

--- bin/exercise_2.dart
+++ bin/exercise_2.dart
@@ -1,37 +1,33 @@
 void main(List<String> args) {
   //
-  List<num> numbers = [7, 2, 2, 5, 10, 3, 41, 43, 35, 87, 9, 8, 5];
+  List<num> numbers = [7, 2, 2, 5, 10, 3, 41, 43, 35, 87, 9, 8, 1, 88, 5];
   print(getSLAndSG(numbers));
 }
 
-//function to get second Lowest and greatest in list
 List<num> getSLAndSG(List<num> numbers) {
   // sort the list
-  numbers.sort(
-    (a, b) => a.compareTo(b),
-  );
+  numbers.sort();
+
   //default consider second element
   num secondSmallest = numbers[1];
 
-  for (int i = 2; i < numbers.length; i++) {
-    if (secondSmallest != numbers[i]) {
-      // check number after
+  // what if 2 or 3 elements as the same following
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[0] != numbers[i]) {
       secondSmallest = numbers[i];
       break;
     }
-    // secondSmallest = numbers[i];
   }
-
   // default consider the before last elment
-  num secondgreatest = numbers[numbers.length - 2];
+  num secondGreatest = numbers[numbers.length - 2];
+
+  // what if 2 or 3 elements as the same following
+  int length = numbers.length;
   for (int i = numbers.length - 2; i >= 0; i--) {
-    if (secondgreatest != numbers[i - 1]) {
-      // check if number before
-      secondgreatest = numbers[i];
+    if (numbers[length - 1] != numbers[i]) {
+      secondGreatest = numbers[i];
       break;
     }
-    // secondgreatest = numbers[i];
   }
-
-  return [secondSmallest, secondgreatest];
+  return [secondSmallest, secondGreatest];
 }
